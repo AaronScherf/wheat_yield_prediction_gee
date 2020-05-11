6ae30db2880f1c4d8c00dbed18af71541f48c236
Aaron Scherf <theaaronscherf@gmail.com>
2020-05-10 21:08:56 -0700

    Create folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Thesis project exploring applications of Google Earth Engine and machine learning models for wheat yield predictions in genotype by environment crop trials.',
+    author='Aaron Scherf',
+    license='MIT',
+)
